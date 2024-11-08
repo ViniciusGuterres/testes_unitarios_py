6aefe46ca5b74377659ec78793c194250ac0152e
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-11-07 21:30:02 -0300

    Final adjustments

--- variacao02.py
+++ variacao02.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import re
 
 class UtilitariosAnaliseTexto:
     def __init__(self,texto):
@@ -17,6 +18,32 @@ class UtilitariosAnaliseTexto:
         """Retorna a palavra mais longa na string. Em caso de empate, retorna a primeira."""
         palavras = self.texto.split()
         return max(palavras, key=len) if palavras else ""
+    
+    def detectar_palavras_chave(self, palavras_chave):
+        """Retorna as palavras-chave encontradas no texto"""
+        palavras_texto = set(re.findall(r'\b\w+\b', self.texto.lower())) 
+        palavras_chave_encontradas = [
+            palavra for palavra in palavras_chave if palavra.lower() in palavras_texto
+        ]
+        return palavras_chave_encontradas
+    
+    def frequencia_palavras(self):
+        palavras = re.findall(r'\b\w+\b', self.texto.lower())
+        return dict(Counter(palavras))
+
+    def eh_anagrama(self, palavra1, palavra2):
+        """Verifica se duas palavras são anagramas entre si, ignorando maiúsculas/minúsculas."""
+        palavra1 = re.sub(r'[^a-zA-Z]', '', palavra1).lower()
+        palavra2 = re.sub(r'[^a-zA-Z]', '', palavra2).lower()
+
+        return sorted(palavra1.lower()) == sorted(palavra2.lower())
+
+    
+    def palavras_mais_comuns(self, n=3):
+        """Retorna as `n` palavras mais comuns em um dicionário, incluindo suas frequências."""
+        freq_dict = self.frequencia_palavras()
+        palavras_ordenadas = sorted(freq_dict.items(), key=lambda x: x[1], reverse=True)
+        return dict(palavras_ordenadas[:n])
 
     def prefixo_comum(self, lista_palavras):
         """Encontra o prefixo comum mais longo em uma lista de palavras ou números."""
